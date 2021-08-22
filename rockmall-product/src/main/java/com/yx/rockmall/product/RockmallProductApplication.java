682f8233ae1fd27f8eaa5301520f07cbf069500b
Yuxuan Chen <chenyuxuang@gmail.com>
2021-08-23 01:44:17 +0800

    Init: Initialize product micro-service module

---
+++ rockmall-product/src/main/java/com/yx/rockmall/product/RockmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.yx.rockmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockmallProductApplication.class, args);
+    }
+
+}
---
+++ rockmall-product/src/test/java/com/yx/rockmall/product/RockmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yx.rockmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
